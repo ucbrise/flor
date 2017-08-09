4d74f8fd182fbc36dfa38090b5da863dc940fa72
Rolando Garcia <rogarcia@berkeley.edu>
2017-08-09 11:54:08 -0700

    Cleaned and commmented code.

--- yaml_parser.py
+++ yaml_parser.py
@@ -1,68 +1,112 @@
 #!/usr/bin/env python
-from pprint import PrettyPrinter
 import yaml
 
-with open("Makefile.yml", 'r') as stream:
+with open("Makefile.yml", "r") as stream:
     try:
         makefile = yaml.load(stream)
     except yaml.YAMLError as exc:
-        print(exc)
+        print exc
 
+# buffer for saving printable strings
 print_table = []
-
 pipeline = makefile["make"]
-for target in pipeline.keys():
-	lhs = target
-	colon = " : "
-	if "tasks" in pipeline[target]:
-		tasks = " ".join(pipeline[target]["tasks"])
+
+"""
+This function takes an Array (possible of arrays)
+and outputs a string, s.t. each element is seperated
+by &&
+
+For example:
+["rm -f *.pkl *.txt"] => "rm -f *.pkl *.txt"
+["source activate py36", ["python $0"], "source deactivate"] 
+	=> "source activate py36 && python $0 && source deactivate"
+
+"""
+def flatten(recipe):
+	for idx, val in enumerate(recipe):
+		if isinstance(val, list):
+			recipe[idx] = flatten(val)
+	return " && ".join(recipe)
+
+"""
+Outputs a string of all the dependencies
+separated by space.
+"""
+def get_rhs(dependencies):
+	
+	if "tasks" in dependencies:
+		tasks = " ".join(dependencies["tasks"])
 	else:
 		tasks = ""
-	if "meta-data" in pipeline[target]:
-		metadata = " ".join(pipeline[target]["meta-data"])
+	
+	if "meta-data" in dependencies:
+		metadata = " ".join(dependencies["meta-data"])
 	else:
 		metadata = ""
-	if "models" in pipeline[target]:
-		models = " ".join(pipeline[target]["models"])
+	
+	if "models" in dependencies:
+		models = " ".join(dependencies["models"])
 	else:
 		models = ""
-	if "data" in pipeline[target]:
-		data = " ".join(pipeline[target]["data"])
+	
+	if "data" in dependencies:
+		data = " ".join(dependencies["data"])
 	else:
 		data = ""
-	if "script" in pipeline[target]:
-		script = pipeline[target]["script"]
+	
+	if "script" in dependencies:
+		script = dependencies["script"]
 	else:
 		script = ""
 
-	rhs = " ".join([elm for elm in [tasks, metadata, models, data, script] if len(elm) > 0])
-	print_table.append(lhs + colon + rhs + "\n")
+	return " ".join([elm for elm in [tasks, metadata, models, data, script] if len(elm) > 0])
+
+"""
+Outputs the recipe to generate the target.
+Substitutes $0 with the script provided
+"""
+def get_recipe(dependencies):
+	
+	if "script" in dependencies:
+		script = dependencies["script"]
+	else:
+		script = ""
 
 	if "args" in pipeline[target]:
 		args = " ".join(pipeline[target]["args"])
 	else:
 		args = ""
-	
+
 	dollar0 = " ".join([elm for elm in [script, args] if len(elm) > 0])
 
 	if "recipe" in pipeline[target]:
-		if isinstance(pipeline[target]["recipe"], list):	
-			recipe = " && ".join(pipeline[target]["recipe"])
-		else:
-			recipe = pipeline[target]["recipe"]
+		recipe = flatten(pipeline[target]["recipe"])
 	else:
 		recipe = ""
+
 
 	if "test" in pipeline[target]:
 		recipe += " && test -f " + " ".join(pipeline[target]["test"])
 
-	recipe = recipe.replace("$0", dollar0)
+	return recipe.replace("$0", dollar0)
+
+# pipeline.keys() is a list of targets to write to the Makefile
+for target in pipeline.keys():
+	lhs = target
+	colon = " : "
+	rhs = get_rhs(pipeline[target])
+	
+	print_table.append(lhs + colon + rhs + "\n")
+
+	recipe = get_recipe(pipeline[target])
+	
 	if len(recipe) > 0:
 		print_table.append("\t" + recipe + "\n")
+	
 	print_table.append("\n")
 
 print_table.append(".PHONY : " + " ".join(makefile["phony"]) + "\n")
 
-with open("myMakefile", "w") as f:
+with open("Makefile", "w") as f:
 	for line in print_table:
 		f.write(line)
