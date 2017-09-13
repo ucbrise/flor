a2d32ef1c28a50fc93916f4207a2ea5ad7ab6939
Rolando Garcia <rogarcia@berkeley.edu>
2017-09-12 20:02:21 -0700

    ML Workflow expressed in python dsl.

---
+++ crawler.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+
+def crawl(in_artifacts, out_artifacts, out_types, data_source):
+	destination = out_artifacts[0].getLocation()
+	# Define the names of each column in the tweets file
+	attribute_names = []
+	attribute_names.append('id')
+	attribute_names.append('tweet')
+	attribute_names.append('place')
+	attribute_names.append('city')
+	attribute_names.append('country')
+	attribute_names.append('code')
+
+	# Define the data type of every element in a column
+	attribute_types = {
+	    'id': np.int32,
+	    'tweet': str,
+	    'place': str,
+	    'city': str,
+	    'country': str,
+	    'code': str
+	}
+
+	# Read the twitter data into a pandas dataframe
+	params = dict(header=None, names=attribute_names, dtype=attribute_types)
+
+	df = pd.read_csv(data_source, **params)
+	df.to_csv(destination, index=False, header=False)
+
+def tr_crawl(in_artifacts, out_artifacts, out_types):
+	crawl(in_artifacts, out_artifacts, out_types, 
+		'../training_tweets.csv')
+
+def te_crawl(in_artifacts, out_artifacts, out_types):
+	crawl(in_artifacts, out_artifacts, out_types, 
+		'../testing_tweets.csv')
--- jarvis.py
+++ jarvis.py
@@ -1,52 +1,70 @@
 #!/usr/bin/env python3
+import os, sys
 
-class Precondition:
+class Artifact:
 
-	def __init__(self):
+	# loc: location
+	# typ: type
+	# parent: each artifact is produced by 1 action
+	def __init__(self, loc, typ, parent):
+		self.loc = loc
+		self.typ = typ
+		self.parent = parent
+		# Need a way to manage versions, possibly with Ground integration
+		self.version = None
+
+		# Now we bind the artifact to its parent
+		self.parent.out_artifacts.append(self)
+		self.parent.out_types.append(typ)
+
+
+	def pull(self):
+		print(self.loc)
+		self.parent.run()
+
+	def getLocation(self):
+		return self.loc
+
+
+	def hasChanged(self):
 		pass
 
-	def check(self):
+	"""
+	We will want to check the loc prefix to decide where to look
+	for existence e.g. http, s3, sftp, etc.
+	No pre-defined prefix, assume local filesystem.
+	"""
+	def exists(self):
+		if not os.path.isfile(self.loc):
+			print(self.loc + " not found.")
+			sys.exit(1)
+
+	"""
+	We assume an open-ended Integrity predicate on each artifact. 
+	The design of this predicate language is TBD
+	"""
+	def isLegal(self):
 		pass
 
-class Postcondition:
-
-	def __init__(self):
+	def stat(self):
 		pass
 
-	def check(self):
-		pass
 
-class Task:
+class Action:
 
-	def __init__(self, func, preconditions, postconditions):
+	def __init__(self, func, in_artifacts=None):
 		self.func = func
-		self.preconditions = preconditions
-		self.postconditions = postconditions
+		self.out_artifacts = []
+		self.out_types = []
+		self.in_artifacts = in_artifacts
+
 
 	def run(self):
-		for precondition in self.preconditions:
-			precondition.check()
-		result = self.func()
-		for postcondition in self.postconditions:
-			postcondition.check()
-		return result
+		if self.in_artifacts:
+			for artifact in self.in_artifacts:
+				artifact.pull()
+		self.func(self.in_artifacts, self.out_artifacts, self.out_types)
 
-class Metadata(Precondition):
+		
 
-	def __init__(self):
-		pass
-
-class Data(Precondition):
-
-	def __init__(self):
-		pass
-
-class Model(Precondition):
-
-	def __init__(self):
-		pass
-
-class Test(Postcondition):
-
-	def __init__(self):
-		pass
+__valid_types__ = {"metadata", "data", "model", "script"}
