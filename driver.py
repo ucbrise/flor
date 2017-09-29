553d553b57b7051b70a6d3ce1c6a8a4b35f33221
Rolando Garcia <rogarcia@berkeley.edu>
2017-09-28 20:11:18 -0700

    saving changes before major change.
    Former-commit-id: b621f63d96c08708270c2ef07a65d54e1e81755f

--- driver.py
+++ driver.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python3
 import jarvis
 
-from crawler import tr_crawl, te_crawl
-from cleaner import clean
-from train_model import train
-from test_model import test
-
+from crawler import tr_crawl
 tr_crawler = jarvis.Action(tr_crawl)
 training_tweets = jarvis.Artifact('training_tweets.csv', tr_crawler)
 
-te_crawler = jarvis.Action(te_crawl)
-testing_tweets = jarvis.Artifact('testing_tweets.csv', te_crawler)
-
+from cleaner import clean
 tr_cleaner = jarvis.Action(clean, [training_tweets])
 clean_training_tweets = jarvis.Artifact('clean_training_tweets.pkl', tr_cleaner)
+
+from train_model import train
+trainer = jarvis.Action(train, [clean_training_tweets])
+intermediary = jarvis.Artifact('intermediary.pkl', trainer)
+
+from crawler import te_crawl
+te_crawler = jarvis.Action(te_crawl)
+testing_tweets = jarvis.Artifact('testing_tweets.csv', te_crawler)
 
 te_cleaner = jarvis.Action(clean, [testing_tweets])
 clean_testing_tweets = jarvis.Artifact('clean_testing_tweets.pkl', te_cleaner)
 
-trainer = jarvis.Action(train, [clean_training_tweets])
-intermediary = jarvis.Artifact('intermediary.pkl', trainer)
-
+from test_model import test
 tester = jarvis.Action(test, [intermediary, clean_testing_tweets])
 model_accuracy = jarvis.Artifact('model_accuracy.txt', tester)
 
