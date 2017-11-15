4b580a96efec302267a56f67c1ad644e3bc3a0f4
Rolando Garcia <rogarcia@berkeley.edu>
2017-11-14 16:46:28 -0800

    bug fixes
    Former-commit-id: 7da0e48e8fbf741f05e0f2bd2bbd598446b63d54

--- plate.py
+++ plate.py
@@ -12,8 +12,13 @@ from mult import multiply
 do_multiply = jarvis.Action(multiply, [ith_param, ith_param2])
 product = jarvis.Artifact('product.txt', do_multiply)
 
+ith_param3 = jarvis.Sample(1.0, 'params3.txt', False)
+
 do_multiply2 = jarvis.Action(multiply, [product, ith_param2])
 product2 = jarvis.Artifact('product2.txt', do_multiply2)
 
 product2.pull()
 product2.plot()
+
+ith_param3.pull()
+ith_param3.plot()
