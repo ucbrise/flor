ce86e130f97161e8504ef7be01d1b955171e513a
Rolando Garcia <rogarcia@berkeley.edu>
2017-11-16 15:41:23 -0800

    De-jarvis-fied the functions
    Former-commit-id: 169f6100208db8aa2dd7b843e1784b6d95a4e906

--- plate.py
+++ plate.py
@@ -12,13 +12,5 @@ from mult import multiply
 do_multiply = jarvis.Action(multiply, [ith_param, ith_param2])
 product = jarvis.Artifact('product.txt', do_multiply)
 
-ith_param3 = jarvis.Sample(1.0, 'params3.txt', False)
-
-do_multiply2 = jarvis.Action(multiply, [product, ith_param2])
-product2 = jarvis.Artifact('product2.txt', do_multiply2)
-
-product2.pull()
-product2.plot()
-
-ith_param3.pull()
-ith_param3.plot()
+product.pull()
+product.plot()
