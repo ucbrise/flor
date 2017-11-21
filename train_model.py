f0b4e4b552be302bedc011d973df9bcb4c357520
Rolando Garcia <rogarcia@berkeley.edu>
2017-11-21 13:22:54 -0800

    updated examples to match new jarvis
    Former-commit-id: fdf8ab67096030df5f144609ddf106f18eae1b38

--- train_model.py
+++ train_model.py
@@ -26,7 +26,43 @@ import jarvis
 abspath = os.path.dirname(os.path.abspath(__file__))
 
 @jarvis.func
-def train(in_artifacts, out_artifacts):
+def train(tweet_df):
+    intermediary = {}
+
+    tweet_df = tweet_df.loc[:, relevant_attributes]
+
+    # Convert string country code to integer country code
+    country_codes = set([i for i in tweet_df["code"]])
+    country_dict = {}
+    for idx, code in enumerate(country_codes):
+        country_dict[code] = idx
+
+    intermediary["country_dict"] = country_dict
+
+    def convert_to_int(country_string):
+        return country_dict[country_string]
+
+    tweet_df.loc[:, "code"] = tweet_df.loc[:, "code"].apply(convert_to_int)
+
+    ## Convert tweet to bag of words for learning
+
+    # Tokenize Text
+    count_vect = CountVectorizer()
+    X_train = count_vect.fit_transform(tweet_df["tweet"])
+
+    intermediary["vectorizer"] = count_vect
+
+    X_train_label = np.array(tweet_df["code"])
+
+    # Train a classifier
+    clf = MultinomialNB().fit(X_train, X_train_label)
+
+    intermediary["classifier"] = clf
+
+    return intermediary
+
+
+def oldtrain(in_artifacts, out_artifacts):
     intermediary = {}
 
     in_artifact = in_artifacts[0]
