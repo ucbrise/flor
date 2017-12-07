b01916a5380da8b76fdfe437a2182c6eb5e5e444
Rolando Garcia <rogarcia@berkeley.edu>
2017-12-06 20:34:18 -0800

    Ray integration
    Former-commit-id: bb5bdf9e7816cdc9b35561d5949e16372ea31d7a

--- jarvis.py
+++ jarvis.py
@@ -10,6 +10,10 @@ from graphviz import Digraph
 from shutil import copyfile
 from shutil import rmtree
 
+from ray.tune import register_trainable
+from ray.tune import grid_search
+from ray.tune import run_experiments
+
 def func(lambdah):
     if Util.jarvisFileIsIpynb():
         filename =  Util.jarvisFile
@@ -18,18 +22,18 @@ def func(lambdah):
     def wrapped_func(in_artifacts, out_artifacts):
         if in_artifacts:
             in_args = []
-            for in_art in [in_art.loc for in_art in in_artifacts]:
+            for in_art in [in_art.loc if Util.isJarvisClass(in_art) else in_art for in_art in in_artifacts]:
                 if Util.isPickle(in_art):
-                    with open(in_art, 'rb') as f:
-                        x = pickle.load(f)
+                    x = Util.unpickle(in_art)
+                elif Util.isCsv(in_art):
+                    x = in_art
+                elif Util.isLoc(in_art):
+                    with open(in_art, 'r') as f:
+                        x = [i.strip() for i in f.readlines() if i.strip()]
+                    if len(x) == 1:
+                        x = x[0]
                 else:
-                    if Util.isCsv(in_art):
-                        x = in_art
-                    else:
-                        with open(in_art, 'r') as f:
-                            x = [i.strip() for i in f.readlines() if i.strip()]
-                        if len(x) == 1:
-                            x = x[0]
+                    x = in_art
                 in_args.append(x)
             outs = lambdah(*in_args)
         else:
@@ -88,6 +92,11 @@ def jarvisFile(loc):
 class Literal:
 
     def __init__(self, v, name=None):
+        """
+
+        :param v: Literal value
+        :param name: must be globally unique per experiment
+        """
         self.v = v
         self.loc = 'ghost_literal_' + str(Util.literalFilenamesAndIncr()) + '.pkl'
         self.__oneByOne__ = False
@@ -95,7 +104,19 @@ class Literal:
         self.n = 1
 
         # The name is used in the visualization and Ground versioning
-        self.name = name
+        if name is None:
+            temp = self.loc.split('.')[0]
+            i = int(temp.split('_')[2])
+            self.name = 'ghost' + str(i)
+            while self.name in Util.literalNames:
+                i += 1
+                self.name = 'ghost' + str(i)
+        else:
+            self.name = name
+            assert name not in Util.literalNames
+        Util.literalNames |= {self.name}
+
+        Util.literalNameToObj[self.name] = self
 
     def forEach(self):
         if not Util.isIterable(self.v):
@@ -249,6 +270,36 @@ class Artifact:
         self.loclist.sort()
         self.scriptNames.sort()
 
+    def parallelPull(self):
+        Util.visited = []
+        driverfile = Util.jarvisFile
+
+        lambdas = []
+        if not Util.isOrphan(self):
+            self.parent.__serialize__(lambdas)
+
+        def exportedExec(config, reporter):
+            for f, names in lambdas:
+                literals = list(map(lambda x: config[x], names))
+                f(literals)
+            reporter(timesteps_total=1)
+
+        config = {}
+        for kee in Util.literalNameToObj:
+            if Util.literalNameToObj[kee].__oneByOne__:
+                config[kee] = grid_search(Util.literalNameToObj[kee].v)
+            else:
+                config[kee] = Util.literalNameToObj[kee].v
+
+        register_trainable('exportedExec', exportedExec)
+
+        run_experiments({
+            Util.jarvisFile.split('.')[0] : {
+                'run': 'exportedExec',
+                'resources': {'cpu': 1, 'gpu': 0},
+                'config': config
+            }
+        })
 
     def pull(self):
 
@@ -318,8 +369,6 @@ class Artifact:
         Util.versioningDirectory = trueVersioningDir
         return out
 
-
-
     def plot(self, rankdir=None):
         # WARNING: can't plot before pulling.
         # Prep globals, passed through arguments
@@ -388,6 +437,37 @@ class Action:
         self.func(self.in_artifacts, self.out_artifacts)
         Util.visited.append(self.funcName + outNames)
 
+    def __serialize__(self, lambdas):
+        outNames = ''
+        namedLiterals = []
+        for out_artifact in self.out_artifacts:
+            outNames += out_artifact.loc
+        if self.funcName + outNames in Util.visited:
+            return
+        if self.in_artifacts:
+            for artifact in self.in_artifacts:
+                if not Util.isOrphan(artifact):
+                    artifact.parent.__serialize__(lambdas)
+                elif type(artifact) == Literal:
+                    namedLiterals.append(artifact.name)
+
+        def _lambda(literals=[]):
+            i = 0
+            args = []
+            for in_art in self.in_artifacts:
+                if type(in_art) == Literal:
+                    args.append(literals[i])
+                    i += 1
+                else:
+                    args.append(in_art)
+            self.func(args, self.out_artifacts)
+
+
+
+        lambdas.append((_lambda, namedLiterals))
+        Util.visited.append(self.funcName + outNames)
+
+
     def __plotWalk__(self, diagram):
         dot = diagram["dot"]
 
@@ -450,31 +530,42 @@ class Action:
 
         return to_list
 
-
-
-
-
 class Util:
 
-    gc = None
-    jarvisFile = 'driver.py'
-    literalFilenames = 0
-    nodes = {}
     edges = []
-    literals = []
+    gc = None
     ghostFiles = set([])
+    jarvisFile = 'driver.py'
+    literals = []
+    literalFilenames = 0
+    literalNames = set([])
+    literalNameToObj = {}
+    nodes = {}
     versioningDirectory = 'jarvis.d'
     visited = []
 
+    @staticmethod
+    def isLoc(loc):
+        try:
+            ext = loc.split('.')[1]
+            return True
+        except:
+            return False
     @staticmethod
     def isJarvisClass(obj):
         return type(obj) == Artifact or type(obj) == Action or type(obj) == Literal
     @staticmethod
     def isPickle(loc):
-        return loc.split('.')[1] == 'pkl'
+        try:
+            return loc.split('.')[1] == 'pkl'
+        except:
+            return False
     @staticmethod
     def isCsv(loc):
-        return loc.split('.')[1] == 'csv'
+        try:
+            return loc.split('.')[1] == 'csv'
+        except:
+            return False
     @staticmethod
     def jarvisFileIsIpynb():
         return Util.jarvisFile.split('.')[1] == 'ipynb'
