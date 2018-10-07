c24782f19178bf6786e65da643c0ed837b80b179
Rolando Garcia <rogarcia@berkeley.edu>
2018-10-06 23:14:44 -0700

    implemented minimal flor syntax

--- flor/decorators.py
+++ flor/decorators.py
@@ -2,9 +2,13 @@
 
 import inspect
 
+import ast
+
 from flor import util
 from flor import global_state
 from sklearn.externals import joblib
+
+from flor.experiment import Experiment
 
 
 def func(foo):
@@ -22,6 +26,57 @@ def func(foo):
         filename = inspect.getsourcefile(foo).split('/')[-1]
 
     return filename, foo.__name__, foo
+
+def track_action(xp_name):
+    """
+    Auto-generates flor Graph
+    Single Action. Many literals in, Many literals out
+    Artifacts not supported
+    :param xp_name:
+    :return:
+    """
+
+    def function_processor(f):
+        tru_f = func(f)
+
+        def callable_function(*pos, **kws):
+
+            # INPUTS
+
+            pos_var_names = [i for i in f.__code__.co_varnames if i != 'kwargs']
+            for kee in kws:
+                assert kee in f.__code__.co_varnames
+            d = {}
+            d.update(kws)
+            for i, positional_arg in enumerate(pos):
+                kee = pos_var_names[i]
+                assert kee not in d
+                d[kee] = positional_arg
+
+            # OUTPUTS
+            _fun = ast.parse(inspect.getsource(f)).body[0]
+            _kees = [i.s for i in [i for i in _fun.body if type(i) == ast.Return][0].value.keys]
+            assert len(_kees) > 0
+
+            with Experiment(xp_name) as ex:
+                input_lits = []
+                output_lits = []
+                for kee in d:
+                    if util.isIterable(d[kee]):
+                        input_lits.append(ex.literalForEach(d[kee], kee))
+                    else:
+                        input_lits.append(ex.literal(d[kee], kee))
+                do_action = ex.action(tru_f, input_lits)
+                for kee in _kees:
+                    output_lits.append(ex.literal(name=kee, parent=do_action))
+
+            pullable_lit = output_lits[0]
+            pullable_lit.plot()
+            pullable_lit.pull()
+
+        return callable_function
+
+    return function_processor
 
 
 def deprecated_func(lambdah):
