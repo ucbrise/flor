cc81ecc63c1de5af5157536ec89e398b9fb86008
Rolando Garcia <rogarcia@berkeley.edu>
2018-10-23 12:25:45 -0700

    expanded examples

--- examples/logger/basic.py
+++ examples/logger/basic.py
@@ -10,20 +10,27 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
 @flor.track_execution
-def main(x, y=None):
+def main(x, y, z):
     # Load the Data
-    movie_reviews = pd.read_json(log.read('data.json'))
+    movie_reviews = pd.read_json(log.read('data.json'), axis=log.parameter(1))
+    movie_reviews2 = pd.read_json(log.read('data2.json'), axis=log.parameter(1))
+
 
     # Do light preprocessing (see if you can extract the lambda transform)
     # movie_reviews appears on the LHS... so it's being modified.
     # Record the modifying transformation
     # What if we just record the line itself?
-    movie_reviews['rating'] = movie_reviews['rating'].map(lambda x: 0 if x < 5 else 1)
+    log.parameter(z)
+    movie_reviews['rating'] = movie_reviews['rating'].map(lambda x: 0 if x < z else 1)
+
+    z = z + 1
+    log.parameter(z)
 
     # Do train/test split-
     X_tr, X_te, y_tr, y_te = train_test_split(movie_reviews['text'], movie_reviews['rating'],
-                                              test_size=log.parameter(0.20),
-                                              random_state=log.parameter(92))
+                                              test_size=log.parameter(x),
+                                              random_state=log.parameter(y))
+
 
     # Vectorize the English sentences
     vectorizer = TfidfVectorizer()
@@ -36,9 +43,8 @@ def main(x, y=None):
         #############################
         clf = RandomForestClassifier(n_estimators=log.parameter(i)).fit(X_tr, y_tr)
         #############################
-
         score = log.metric(clf.score(X_te, y_te))
 
     print(score)
 
-print(main(1, 20))
+print(main(0.2, 92, 5))
