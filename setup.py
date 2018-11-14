d47728bd258e500d36c68d511fc3b4fee2baa583
Rolando Garcia <rogarcia@berkeley.edu>
2018-11-14 15:58:38 -0800

    Preparing install package

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", 'r') as f:
+    requirements = f.read().split('\n')
+
+setuptools.setup(
+     name='flor',
+     version='0.0.0-alpha',
+     author="Rolando Garcia",
+     author_email="rogarcia@berkeley.edu",
+     description="A Context Management System that feels like a logger",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/ucbrise/flor",
+     packages=setuptools.find_packages(),
+     install_reqs = requirements
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: Apache Software License",
+         "Operating System :: OS Independent",
+     ],
+ )
