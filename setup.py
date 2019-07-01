7e5483f2d9dd5819ed271a9257ca5f95ed9c4d6f
Rolando Garcia <rogarcia@berkeley.edu>
2019-07-01 16:54:14 -0700

    making tests a module

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
      long_description=long_description,
      long_description_content_type="text/markdown",
      url="https://github.com/ucbrise/flor",
-     packages=setuptools.find_packages(),
+     packages=[p for p in setuptools.find_packages() if p != 'tests'],
      install_requires = requirements,
      classifiers = [
          "Programming Language :: Python :: 3",
