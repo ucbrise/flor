eef0c2b37ec7901cdc13699e3418cb6ede61a822
Bobby Yan <bobbyy@berkeley.edu>
2019-08-01 12:11:03 -0700

    collect more contexts during execution

--- flor/logs_manipulator/open_log.py
+++ flor/logs_manipulator/open_log.py
@@ -1,10 +1,16 @@
-from flor.constants import *
-from flor.face_library.flog import Flog
-from flor.utils import cond_mkdir, refresh_tree, cond_rmdir
-from flor.stateful import get, put, start
-import os
 import datetime
 import json
+import re
+import uuid
+
+import git
+import boto3
+
+from flor.constants import *
+from flor.face_library.flog import Flog
+from flor.stateful import put, start
+from flor.utils import cond_mkdir, refresh_tree, cond_rmdir
+
 
 class OpenLog:
 
@@ -21,7 +27,43 @@ class OpenLog:
             put('depth_limit', depth_limit)
 
         session_start = {'session_start': format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))}
-        log_file.write(json.dumps(session_start) + '\n')
+
+        # MAC address
+        MAC_addr = {'MAC_address': ':'.join(re.findall('..?', '{:x}'.format(uuid.getnode())))}
+
+        to_write = [session_start, MAC_addr]
+
+        # Get EC2 instance type
+        try:
+            ec2 = boto3.resource('ec2')
+            for i in ec2.instances.all():
+                to_write.append({'EC2_instance_type': i.instance_type})
+        except:
+            to_write.append({'EC2_instance_type': 'None'})
+
+        # User info from Git
+        class GitConfig(git.Repo):
+            def __init__(self, *args, **kwargs):
+                #
+                # Work around the GitPython issue #775
+                # https://github.com/gitpython-developers/GitPython/issues/775
+                #
+                self.git_dir = os.path.join(os.getcwd(), ".git")
+                git.Repo.__init__(self, *args, **kwargs)
+
+        r = GitConfig().config_reader()
+        user_name = r.get_value('user', 'name')
+        user_email = r.get_value('user', 'email')
+        to_write.append({'git_user_name': user_name})
+        to_write.append({'git_user_email': user_email})
+
+        # System's userid
+        import getpass
+        user_id = getpass.getuser()
+        to_write.append({'user_id': user_id})
+
+        for x in to_write:
+            log_file.write(json.dumps(x) + '\n')
         log_file.flush()
         log_file.close()
 
