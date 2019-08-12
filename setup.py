7a71f8fd15f0e935f8722c941ce1cf51620375f6
Rolando Garcia <rogarcia@berkeley.edu>
2019-08-12 16:06:17 -0700

    bug fix causing infinite recursion

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt", 'r') as f:
 
 setuptools.setup(
      name='pyflor',
-     version='0.0.3a30',
+     version='0.0.3a31',
      author="Rolando Garcia",
      author_email="rogarcia@berkeley.edu",
      description="A diagnostics and sharing system for ML",
