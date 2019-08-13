bb02cfc5d8de6f8eaabd519bc70cf946f0c22304
Rolando Garcia <rogarcia@berkeley.edu>
2019-08-13 13:17:58 -0700

    excluding highlight_plugin directory from pip installation

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
      long_description=long_description,
      long_description_content_type="text/markdown",
      url="https://github.com/ucbrise/flor",
-     packages=[p for p in setuptools.find_packages() if p != 'tests'],
+     packages=[p for p in setuptools.find_packages() if p != 'tests' or p != 'highlight_plugin'],
      install_requires = requirements,
      classifiers = [
          "Programming Language :: Python :: 3",
