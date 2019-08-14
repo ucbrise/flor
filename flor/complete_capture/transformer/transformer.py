19df2cd3d03821e2ba5ad2828612cd496e96434e
Rolando Garcia <rogarcia@berkeley.edu>
2019-08-14 10:50:18 -0700

    fixed bug: import flor in correct location, sensitive to __future__ imports

--- flor/complete_capture/transformer/transformer.py
+++ flor/complete_capture/transformer/transformer.py
@@ -26,6 +26,70 @@ class Transformer(ast.NodeTransformer):
         def get_foot(self):
             return self.client_root.parse_foot()
 
+        def proc_imports(self, node):
+            """
+            Helper method for generic_visit
+            :param node:
+            :return:
+            """
+
+            def is_future(node):
+                if isinstance(node, ast.ImportFrom):
+                    return node.module == '__future__'
+                elif isinstance(node, ast.Import):
+                    for each in node.names:
+                        if is_future(each):
+                            return True
+                    return False
+                elif isinstance(node, ast.alias):
+                    return node.name == '__future__'
+                else:
+                    return False
+
+            contains_future = any(map(is_future, node.body))
+
+            first_import = None
+            last_import = None
+            if contains_future:
+                for i, child in enumerate(node.body):
+                    if first_import is None and last_import is None and not (
+                            isinstance(child, ast.ImportFrom) or isinstance(child, ast.Import)):
+                        continue
+                    elif first_import is None and last_import is None and (
+                            isinstance(child, ast.ImportFrom) or isinstance(child, ast.Import)):
+                        first_import = i
+                    elif first_import is not None and last_import is None and (
+                            isinstance(child, ast.ImportFrom) or isinstance(child, ast.Import)):
+                        continue
+                    elif first_import is not None and last_import is None and not (
+                            isinstance(child, ast.ImportFrom) or isinstance(child, ast.Import)):
+                        last_import = i - 1
+                        break
+                    else:
+                        raise RuntimeError(
+                            "Case not handled. [first_import is None, {}]. [last_import is None, {}]. [is import, {}]".format(
+                                first_import is None, last_import is None,
+                                isinstance(child, ast.ImportFrom) or isinstance(child, ast.Import)
+                            ))
+
+                if last_import is None:
+                    # This is true whenever the full file is nothing but imports
+                    return node
+
+                prefix = node.body[0:last_import + 1]
+                postfix = node.body[last_import + 1:]
+
+                heads = self.get_heads()
+
+                heads.extend(postfix)
+                prefix.extend(heads)
+                node.body = prefix
+                node.body.extend(self.get_foot())
+            else:
+                heads = self.get_heads()
+                heads.extend(node.body)
+                node.body = heads
+                node.body.extend(self.get_foot())
 
     def __init__(self, filepath=''):
         super().__init__()
@@ -150,63 +214,6 @@ class Transformer(ast.NodeTransformer):
         new_node.body = body
         return new_node
 
-    def _proc_imports(self, node, header_obj):
-        """
-        Helper method for generic_visit
-        :param node:
-        :param header_obj:
-        :return:
-        """
-        contains_imports = False
-
-        for child in node.body[0:3]:
-            if isinstance(child, ast.ImportFrom) or isinstance(child, ast.Import):
-                contains_imports = True
-                break
-
-        first_import = None
-        last_import = None
-        if contains_imports:
-            for i, child in enumerate(node.body):
-                if first_import is None and last_import is None and not (
-                        isinstance(child, ast.ImportFrom) or isinstance(child, ast.Import)):
-                    continue
-                elif first_import is None and last_import is None and (
-                        isinstance(child, ast.ImportFrom) or isinstance(child, ast.Import)):
-                    first_import = i
-                elif first_import is not None and last_import is None and (
-                        isinstance(child, ast.ImportFrom) or isinstance(child, ast.Import)):
-                    continue
-                elif first_import is not None and last_import is None and not (
-                        isinstance(child, ast.ImportFrom) or isinstance(child, ast.Import)):
-                    last_import = i - 1
-                    break
-                else:
-                    raise RuntimeError(
-                        "Case not handled. [first_import is None, {}]. [last_import is None, {}]. [is import, {}]".format(
-                            first_import is None, last_import is None,
-                            isinstance(child, ast.ImportFrom) or isinstance(child, ast.Import)
-                        ))
-
-            if last_import is None:
-                # This is true whenever the full file is nothing but imports
-                return node
-
-            prefix = node.body[0:last_import + 1]
-            postfix = node.body[last_import + 1:]
-
-            heads = header_obj.get_heads()
-
-            heads.extend(postfix)
-            prefix.extend(heads)
-            node.body = prefix
-            node.body.extend(header_obj.get_foot())
-        else:
-            heads = header_obj.get_heads()
-            heads.extend(node.body)
-            node.body = heads
-            node.body.extend(header_obj.get_foot())
-
     def generic_visit(self, node):
         for field, old_value in ast.iter_fields(node):
             if isinstance(old_value, list):
@@ -245,7 +252,7 @@ class Transformer(ast.NodeTransformer):
                 old_value[:] = new_values
                 if header_obj is not None:
                     assert isinstance(node, ast.Module)
-                    self._proc_imports(node, header_obj)
+                    header_obj.proc_imports(node)
             elif isinstance(old_value, ast.AST):
                 new_node = self.visit(old_value)
                 if new_node is None:
