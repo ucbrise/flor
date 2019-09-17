5950903b0e01ad2efcb279bd809ac879cbb582f3
Bobby Yan <bobbyy@berkeley.edu>
2019-09-17 16:20:02 -0700

    slightly more general fix

--- flor/complete_capture/transformer/transformer.py
+++ flor/complete_capture/transformer/transformer.py
@@ -149,7 +149,10 @@ class Transformer(ast.NodeTransformer):
                                 isinstance(value.targets[0], ast.Name) and \
                                 value.targets[0].id == '__all__' and \
                                 'dir()' in astor.to_source(value.value):
-                            value.value.generators[0].ifs = [ast.parse(astor.to_source(value.value.generators[0].ifs[0]).replace('\n', '') + "and s != 'flog'").body[0].value]
+                            value.value.generators[0].ifs = [
+                                ast.parse(astor.to_source(value.value.generators[0].ifs[0]).replace('\n', '')
+                                          + "and {} != 'flog'".format(value.value.generators[0].target.id)).body[0].value
+                            ]
                         # OUTPUT ASSIGN STATEMENT
                         value = self.visit(Assign(value, self.relative_counter).parse())
                         new_values.append(value)
