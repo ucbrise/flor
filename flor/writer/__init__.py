ba0214da67e59a4ea3eb23b8ac1ec1bbb94a42bf
Eric Liu <rickchang@berkeley.edu>
2019-11-02 13:24:50 -0700

    polishing and adding comments

--- flor/writer/__init__.py
+++ flor/writer/__init__.py
@@ -47,13 +47,13 @@ class Writer:
     @staticmethod
     def serialize(obj):
         if isinstance(obj, Tensor):
-            return obj.clone()
+            return obj.clone()  # if object is tensor, call tensor's copy method
         if not isinstance(obj, (int, float, bool, str)):
-            try:
+            try:  # int, float, bool, str are primitives
                 Writer.serializing = True
                 return copy.deepcopy(obj)
             except:
-                try:
+                try:  # if deepcopy fails, simply pickle or fail to serialize
                     return str(cloudpickle.dumps(obj))
                 except:
                     return "ERROR: failed to serialize"
@@ -66,19 +66,20 @@ class Writer:
     def write(obj):
         obj['global_lsn'] = Writer.lsn
         Writer.write_buffer.append(obj)
-        Writer.lsn += 1
+        Writer.lsn += 1  # append to buffer and increment lsn
         if len(Writer.write_buffer) >= Writer.max_buffer or Writer.fork_now:
-            Writer.forked_write()
+            Writer.forked_write()  # if buffer exceeds a certain size, or fork_now is triggered
+            # note: fork_now is there as a mechanism for forcing fork, we aren't using it yet
 
     @staticmethod
     def forked_write():
         Writer.fork_now = False
         pid = os.fork()
         if not pid:
-            os.nice(1)
+            os.nice(1)  # child process gets lower priority and starts flushing
             Writer.serializing = True
             for each in Writer.write_buffer:
-                if 'value' in each:
+                if 'value' in each:  # the dict can have 'value' or 'state'
                     each['value'] = str(cloudpickle.dumps(each['value']))
                 else:
                     each['state'] = str(cloudpickle.dumps(each['state']))
@@ -87,49 +88,50 @@ class Writer:
             Writer.serializing = False
             os._exit(0)
         else:
-            Writer.write_buffer = []
+            Writer.write_buffer = []  # parent process resets buffer
 
 
     @staticmethod
     def flush():
         if Writer.write_buffer:
-            Writer.forked_write()
+            Writer.forked_write()  # at the end of flor execution, flushes buffer to disk
 
 
     @staticmethod
     def store(obj):
         # Store the object in the memo
         if isinstance(obj, dict):
-            obj = StateWrapper(obj).get() # this moves all tensors to cpu
-            # there should be a test that accepts or rejects state dicts
+            Writer.dict_check(obj)  # this moves all tensors to cpu
+            # later we should have even more special handling for state dicts
         elif isinstance(obj, Tensor):
-            obj = obj.cpu()
+            obj = obj.cpu() # tensors are moved to cpu before copying
         d = {
             'source': 'store',
             'value': Writer.serialize(obj)
         }
         Writer.write(d)
 
-    # @staticmethod
-    # def store_dict(obj: dict):
-    #     #if this is a dict, it would only have tensors in it
-    #     #however, if there are more than just tensors, we must copy
-    #     # for k, v in obj.items():
-    #     #     if isinstance(v, Tensor):
-    #     #         obj[k] = v.cpu()
-    #     #     else:
-    #     #         obj[k] = Writer.serialize(obj) #this deepcopies or serializes the output
-    #     d = {
-    #         'source': 'store',
-    #         'value': StateWrapper(obj)
-    #     }
-    #     return d
+    @staticmethod
+    def dict_check(data):
+        for k, v in data.items():  # check every item to see if its a tensor
+            if isinstance(v, dict):
+                Writer.dict_check(data[k])  # recursively check another dict
+            elif isinstance(v, list):
+                Writer.list_check(data[k])  # recursively check a list
+            elif isinstance(v, Tensor):
+                data[k] = v.cpu()  # convert the tensor to cpu
+                # this might need to be modified later because it is not general purpose
 
-    # @staticmethod
-    # def store_tensor(obj: Tensor):
-    #     #special handling for tensors
-    #     on_cpu = obj.cpu()
-    #     return {'source': 'store', 'value': on_cpu}
+    @staticmethod
+    def list_check(data):
+        for x in range(len(data)):  # check every item
+            if isinstance(data[x], dict):
+                Writer.dict_check(data[x])  # check dict
+            elif isinstance(data[x], list):
+                Writer.list_check(data[x])  # check list
+            elif isinstance(data[x], Tensor):
+                data[x] = data[x].cpu()  # convert the tensor to cpu
+                # this might need to be modified later because it is not general purpose
 
     @staticmethod
     def load():
