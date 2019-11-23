0d8f3a25ad7dd6e8ff875d4f4ee59d54c8d7ac8e
Rolando Garcia <rogarcia@berkeley.edu>
2019-11-22 17:50:08 -0800

    Trying fork with no-op

--- flor/writer/__init__.py
+++ flor/writer/__init__.py
@@ -84,16 +84,16 @@ class Writer:
         cuda.synchronize()
         pid = os.fork()
         if not pid:
-            path = LOG_PATH.split('.')
-            path.insert(-1, str(Writer.lsn))
-            path = '.'.join(path)
-            fd = open(path, 'w')
-            os.nice(1)  # child process gets lower priority and starts flushing
-            for each in Writer.write_buffer:
-                if 'value' in each and not isinstance(each['value'], str):  # the dict can have 'value' or 'state'
-                    each['value'] = Writer.serialize(each['value'])
-                fd.write(json.dumps(each) + '\n')
-            fd.close()
+            # path = LOG_PATH.split('.')
+            # path.insert(-1, str(Writer.lsn))
+            # path = '.'.join(path)
+            # fd = open(path, 'w')
+            # os.nice(1)  # child process gets lower priority and starts flushing
+            # for each in Writer.write_buffer:
+            #     if 'value' in each and not isinstance(each['value'], str):  # the dict can have 'value' or 'state'
+            #         each['value'] = Writer.serialize(each['value'])
+            #     fd.write(json.dumps(each) + '\n')
+            # fd.close()
             os._exit(0)
         else:
             Writer.write_buffer = []  # parent process resets buffer
