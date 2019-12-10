cd43c99a162c4ed693d53cf111bf73ab3fe8a692
Rolando Garcia <rogarcia@berkeley.edu>
2019-12-10 13:24:19 -0800

    Minor bug fix

--- flor/utils.py
+++ flor/utils.py
@@ -30,8 +30,7 @@ def cond_rmdir(path):
 
 
 def fprint(dir_tree_list, device_id):
-    root_path = os.path.join(*dir_tree_list)
-
+    root_path = os.path.sep + os.path.join(*dir_tree_list)
     def write(s):
         with open(os.path.join(root_path, "flor_output_{}.txt".format(device_id)), 'a') as f:
             f.write(s + '\n')
