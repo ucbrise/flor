6d8bda4188233b48bd5ee10870bb5c7641a6cf82
Rolando Garcia <rogarcia@berkeley.edu>
2020-04-10 12:35:55 -0700

    Weak init for sampling runs with sparse chkpts

--- flor/utils.py
+++ flor/utils.py
@@ -50,15 +50,16 @@ def fprint(dir_tree_list, device_id):
 
     return write
 
-def get_partitions(num_epochs, num_gpus, pretraining, period):
+def get_partitions(num_epochs, num_partitions, pretraining, period):
     # Roundrobin allocation with pipelining
+    assert num_partitions <= num_epochs
     if pretraining:
         del period
-        partitions = [[] for _ in range(num_gpus)]
+        partitions = [[] for _ in range(num_partitions)]
         for epoch in range(num_epochs):
-            partitions[epoch % num_gpus].append(-1)
+            partitions[epoch % num_partitions].append(-1)
         i = 0
-        for j in range(num_gpus):
+        for j in range(num_partitions):
             for k in range(len(partitions[j])):
                 partitions[j][k] = i
                 i += 1
@@ -76,16 +77,20 @@ def get_partitions(num_epochs, num_gpus, pretraining, period):
             stop = min((i+1)*period, num_epochs)
             range_regions.append(range(start, stop))
             i+=1
-        partitions = [[] for _ in range(num_gpus)]
+        partitions = [[] for _ in range(num_partitions)]
         for range_element in range(len(range_regions)):
             #roundrobin work allocation, early epochs first
-            partitions[range_element % num_gpus].append(-1)
-        for j in range(num_gpus):
+            partitions[range_element % num_partitions].append(-1)
+        for j in range(num_partitions):
             for k in range(len(partitions[j])):
                 partitions[j][k] = range_regions.pop(0)
         assert len(range_regions) == 0
         partitions = [range(rs[0].start, rs[-1].stop) if rs else [] for rs in partitions]
-        return partitions
+        if num_partitions < num_epochs:
+            return partitions
+        else:
+            # For when you sample a Fine-tuning run with sparse checkpoints
+            return [range(p.start, s+1) for p in partitions for s in p]
 
 
 
