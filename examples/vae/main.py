ee150f1a4617057261fb747e6e5ce244f81180ac
Bobby Yan <bobbyy@berkeley.edu>
2020-11-21 18:43:02 -0800

    fix requirements

--- examples/vae/main.py
+++ examples/vae/main.py
@@ -112,17 +112,19 @@ def test(epoch):
     model.eval()
     test_loss = 0
 
-    with torch.no_grad():
-        for i, (data, _) in enumerate(test_loader):
-            data = data.to(device)
-            recon_batch, mu, logvar = model(data)
-            test_loss += loss_function(recon_batch, data, mu, logvar).item()
-            if i == 0:
-                n = min(data.size(0), 8)
-                comparison = torch.cat([data[:n],
-                                      recon_batch.view(args.batch_size, 1, 28, 28)[:n]])
-                save_image(comparison.cpu(),
-                         'results/reconstruction_' + str(epoch) + '.png', nrow=n)
+    if flor.SkipBlock.start():
+        with torch.no_grad():
+            for i, (data, _) in enumerate(test_loader):
+                data = data.to(device)
+                recon_batch, mu, logvar = model(data)
+                test_loss += loss_function(recon_batch, data, mu, logvar).item()
+                if i == 0:
+                    n = min(data.size(0), 8)
+                    comparison = torch.cat([data[:n],
+                                        recon_batch.view(args.batch_size, 1, 28, 28)[:n]])
+                    save_image(comparison.cpu(),
+                            'results/reconstruction_' + str(epoch) + '.png', nrow=n)
+    test_loss = flor.SkipBlock.end(test_loss)
 
     test_loss /= len(test_loader.dataset)
     print('====> Test set loss: {:.4f}'.format(test_loss))
