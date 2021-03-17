42de3da22664028646102f0b5512afeee61b65ee
Bobby Yan <bobbyy@berkeley.edu>
2021-03-17 11:57:02 -0700

    add logger

--- flor/journal/file.py
+++ flor/journal/file.py
@@ -1,6 +1,7 @@
 from .entry import DataVal, DataRef, Bracket, EOF, make_entry
 
 from flor import shelf
+from ..logger import Logger
 
 import json
 import pathlib
@@ -8,6 +9,13 @@ from typing import Union, List
 
 entries: List[Union[DataRef, DataVal, Bracket, EOF]] = []
 
+def _proc_log_record(log_record):
+    if isinstance(log_record, DataRef):
+        log_record.set_ref_and_dump(shelf.get_pkl_ref())
+    return json.dumps(log_record.jsonify()) + pathlib.os.linesep
+
+entries_logger = Logger()
+entries_logger.register_pre(_proc_log_record)
 
 def read():
     with open(shelf.get_index(), 'r') as f:
@@ -17,16 +25,12 @@ def read():
 
 
 def feed(journal_entry: Union[DataRef, DataVal, Bracket, EOF]):
-    entries.append(journal_entry)
-
+    if entries_logger.path is None:
+        entries_logger.set_path(shelf.get_index())
+    entries_logger.append(journal_entry)
 
 def write():
-    with open(shelf.get_index(), 'w') as f:
-        for log_record in entries:
-            if isinstance(log_record, DataRef):
-                log_record.set_ref_and_dump(shelf.get_pkl_ref())
-            f.write(json.dumps(log_record.jsonify()) + pathlib.os.linesep)
-    entries[:] = []
+    entries_logger.flush()
 
 
 def merge():
