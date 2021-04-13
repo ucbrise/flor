59f17b60bee75f997268641cfba0feb6e0579cc9
Rolando Garcia <rogarcia@berkeley.edu>
2021-04-12 21:01:27 -0700

    logger merges  files

--- flor/logger/logger.py
+++ flor/logger/logger.py
@@ -3,15 +3,31 @@ from .future import Future
 from .. import shelf
 
 import os
+import glob
 from pathlib import PurePath
 from typing import List, Tuple, Any
 
+#%%
+import glob
+from pathlib import Path
+d = Path.home() / 'sd_card' / 'sandbox' / 'foo.txt'
+read_files = glob.glob(d.with_name(d.stem + '_*' +  d.suffix).as_posix())
+read_files
+
+#%%
 
 class Logger:
     def __init__(self, path=None, buf_size=None):
         self.path = PurePath(path) if path is not None else None
         self.buffer = Buffer() if buf_size is None else Buffer(buf_size)
         self.flush_count = 0
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *exc):
+        self.close()
+        return False
 
     def set_path(self, path):
         self.path = PurePath(path)
@@ -21,6 +37,19 @@ class Logger:
         self.buffer.append(o)
         if self.buffer.is_full():
             self.flush()
+
+    def close(self):
+        p = self.path.with_name(self.path.stem + '_*' + self.path.suffix)
+        with open(self.path, 'wb') as out_f:
+            for pi in glob.glob(p.as_posix()):
+                with open(pi, 'rb') as in_f:
+                    out_f.write(in_f.read()) 
+                os.remove(pi)
+        latest = shelf.get_latest()        
+        if latest.exists():
+            latest.unlink()
+        latest.symlink_to(self.path)
+        # TODO: spool
 
     def flush(self):
         self.flush_count += 1
@@ -82,3 +111,5 @@ class Buffer:
 
 # Add feed and write
 # there used to be a tree feed entry. Why?
+
+# %%
