4fc9d444c3af652ca3ab5c6e8ce6cfc8e64a862a
Rolando Garcia <rogarcia@berkeley.edu>
2021-07-01 19:40:20 +0000

    True refactor

--- examples/linear.py
+++ examples/linear.py
@@ -33,12 +33,12 @@ transform = transforms.Compose(
 trainset = torchvision.datasets.MNIST(
     root="./mnist", train=True, download=True, transform=transform
 )
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, num_workers=4)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, num_workers=4)
 
 testset = torchvision.datasets.MNIST(
     root="./mnist", train=False, download=True, transform=transform
 )
-testloader = torch.utils.data.DataLoader(testset, batch_size=64, num_workers=4)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4, num_workers=4)
 
 
 def eval(net):
