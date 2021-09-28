e4738fd1a2bf5942614fc6e6e8cb65b1863cbfd4
Rolando Garcia <rlnsanz@users.noreply.github.com>
2021-09-28 23:44:51 +0000

    test success

--- tests/training/linear.py
+++ tests/training/linear.py
@@ -1,10 +1,10 @@
+import flor
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
-import flor
 
 
 class Net(nn.Module):
@@ -28,12 +28,12 @@ transform = transforms.Compose(
 trainset = torchvision.datasets.MNIST(
     root="./mnist", train=True, download=True, transform=transform
 )
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, num_workers=2)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, num_workers=2)  # type: ignore
 
 testset = torchvision.datasets.MNIST(
     root="./mnist", train=False, download=True, transform=transform
 )
-testloader = torch.utils.data.DataLoader(testset, batch_size=4, num_workers=2)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4, num_workers=2)  # type: ignore
 
 
 def eval(net):
