ce09de73c985e7f022759e421c53cafa691e8691
Rolando Garcia <rlnsanz@users.noreply.github.com>
2021-09-30 21:09:50 +0000

    lightweight version control

--- flor/iterator.py
+++ flor/iterator.py
@@ -1,8 +1,11 @@
+from typing import Iterable, Union
+
+from git.repo import Repo
+from git.exc import InvalidGitRepositoryError
+
 from . import flags
 from . import shelf
 from .skipblock import SkipBlock
-
-from typing import Union, Iterable
 
 
 def it(value: Union[Iterable, bool]):
@@ -69,6 +72,8 @@ def _deferred_init(_nil=[]):
     """
     if not _nil:
         assert flags.NAME is not None
+        commit = _save_versions()
+        print(commit)
         shelf.mk_job(flags.NAME)
         SkipBlock.bind()
         if flags.REPLAY:
@@ -77,6 +82,39 @@ def _deferred_init(_nil=[]):
             SkipBlock.logger.set_path(shelf.get_index())
             assert SkipBlock.logger.path is not None
         _nil.append(True)
+
+
+def _save_versions() -> str:
+    try:
+        repo = Repo()
+    except InvalidGitRepositoryError:
+        repo = Repo.init()
+
+    if not repo.branches:
+        repo.index.commit("initial commit")
+
+    active_branch = repo.active_branch
+    repo.git.stash("-u")
+    if flags.NAME not in [b.name for b in repo.branches]:
+        repo.create_head(flags.NAME)
+
+    repo.git.checkout(flags.NAME)
+    stash_msg_list: str = repo.git.stash("list")
+
+    if stash_msg_list:
+        repo.git.stash("apply")
+
+    repo.git.add("-A")
+    commit = repo.index.commit(
+        f"flor::{active_branch.name}@{active_branch.commit.hexsha}"
+    )
+    commit_sha = commit.hexsha
+
+    repo.git.checkout(active_branch.name)
+    if stash_msg_list:
+        repo.git.stash("pop")
+
+    return commit_sha
 
 
 __all__ = ["it"]
