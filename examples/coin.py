3048c6168f64bbe14f130127302451908c4c30e8
Rolando Garcia <rogarcia@berkeley.edu>
2022-10-09 12:42:23 -0700

    updated examples

--- examples/coin.py
+++ examples/coin.py
@@ -1,11 +1,13 @@
+from flor import Flor
 import flor
+
 import random
 import datetime
 
 print("Hello World")
 
-coin = flor.pin("coin", "HEADS" if random.randint(0, 1) else "TAILS")
-now = flor.pin("time", str(datetime.datetime.now()))
+coin = flor.recall("coin", "HEADS" if random.randint(0, 1) else "TAILS")
+now = flor.recall("time", str(datetime.datetime.now()))
 
 print(f"You flipped a {coin} on {now}")
-flor.report_end()
+Flor.commit()
