fc38af9cdd3f526455fce87818e004b4a2990f89
Rolando Garcia <rogarcia@berkeley.edu>
2022-11-09 16:38:33 -0800

    adding dpk

--- flor/__init__.py
+++ flor/__init__.py
@@ -1,7 +1,7 @@
 from . import flags
-from .kits import MTK, load_kvs
+from .kits import MTK, DPK, load_kvs
 from .pin import pin as recall, log, pkl
 
 flags.Parser.parse()
 
-__all__ = ["MTK", "flags", "log", "recall", "load_kvs", "pkl"]
+__all__ = ["MTK", "DPK", "flags", "log", "recall", "load_kvs", "pkl"]
