90c2d294f4f62085f0d70f1bfabf3301a9adf9ed
Rolando Garcia <rogarcia@berkeley.edu>
2023-01-30 13:57:56 -0700

    bug patch

--- flor/__main__.py
+++ flor/__main__.py
@@ -87,7 +87,12 @@ elif sys.argv[1] == "unpack":
     for version in commits:
         r.git.checkout(version)
         hexsha, message = version.hexsha, version.message
-        _, tstamp_json = message.split("::")  # type: ignore
+        messages = message.split("::")  # type: ignore
+        if len(messages) != 2:
+            print(f"Did not parse >>{messages}<<")
+            continue
+        else:
+            _, tstamp_json = messages
         try:
             shutil.copy2(".replay.json", os.path.join(replay_jsons, tstamp_json))  # type: ignore
             print(f'copied {(str(version.hexsha) + "::" + str(tstamp_json))}')
