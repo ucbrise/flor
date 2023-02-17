8e64d55fb0f9cbba4ccae88e7a9d725806dcc41a
Rolando Garcia <rogarcia@berkeley.edu>
2023-02-17 11:31:08 -0700

    preparing pivot logic

--- flor/query/__init__.py
+++ flor/query/__init__.py
@@ -17,17 +17,21 @@ def log_records(cache_dir=None):
         if path.suffix == ".csv":
             with open(path, "r") as f:
                 data.extend(list(csv.DictReader(f)))
-    facts = pd.DataFrame(data).astype(
-        {
-            "projid": str,
-            "tstamp": np.datetime64,
-            "vid": str,
-            "epoch": int,
-            "step": int,
-            "name": str,
-            "value": object,
-        }
-    ).sort_values(by=["tstamp", "epoch", "step"])
+    facts = (
+        pd.DataFrame(data)
+        .astype(
+            {
+                "projid": str,
+                "tstamp": np.datetime64,
+                "vid": str,
+                "epoch": int,
+                "step": int,
+                "name": str,
+                "value": object,
+            }
+        )
+        .sort_values(by=["tstamp", "epoch", "step"])
+    )
     return facts
 
 
@@ -35,4 +39,25 @@ def full_pivot():
     global facts
     if facts is None:
         facts = log_records()
-    return facts
+
+    data_prep = facts.query("epoch == -1")
+    data_prep_names = set(data_prep["name"])
+    outer_loop_gb = facts.groupby(by=["name", "vid", "epoch"])
+    outer_loop_names = set([])
+    for rowid, agg in outer_loop_gb.count()["value"].items():
+        name, hexsha, _ = tuple(rowid) #type: ignore
+        if name not in data_prep_names and agg == 1:
+            outer_loop_names |= {
+                name,
+            }
+    inner_loop_names = set(
+        [
+            name
+            for name in facts["name"]
+            if name not in data_prep_names and name not in outer_loop_names
+        ]
+    )
+
+    print(
+        f"\nData prep: {data_prep_names},\nouter_loop: {outer_loop_names},\ninner_loop: {inner_loop_names}"
+    )
---
+++ flor/query/pivot.py
@@ -0,0 +1,10 @@
+def data_prep_pivot(df, data_prep_names):
+    ...
+
+
+def outer_loop_pivot(df, outer_loop_names):
+    ...
+
+
+def inner_loop_pivot(df, inner_loop_names):
+    ...
