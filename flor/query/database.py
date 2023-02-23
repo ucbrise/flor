179c7307d32fd62d3b77f03bbe87f2b6d9f9a442
Rolando Garcia <rogarcia@berkeley.edu>
2023-02-23 12:33:39 -0700

    implementing db initialization

--- flor/query/database.py
+++ flor/query/database.py
@@ -10,6 +10,35 @@ SUFFIX = ".db"
 def start_db(projid: str):
     (dir_n, _) = projid.split("_")
     dbp = home_shelf.florin / Path(dir_n).with_suffix(SUFFIX)
+    is_first_start = not dbp.exists()
     assert State.db_conn is None, "Did you try to start_db more than once?"
     State.db_conn = sqlite3.connect(dbp)
+    if is_first_start:
+        init_db()
 
+
+def init_db():
+    """
+    Initializes the database
+    1. Creates log_records
+    2. Defines pivot views
+    3. Updates metadata
+    """
+    assert State.db_conn is not None
+    cur = State.db_conn.cursor()
+    cur.executescript(
+        """
+    BEGIN;
+    CREATE TABLE watermark(projid text, commitsha text);
+    CREATE TABLE log_records(
+        projid text,
+        tstamp text,
+        vid text,
+        epoch integer,
+        step integer,
+        name text,
+        value text
+        );
+    COMMIT;
+    """
+    )
