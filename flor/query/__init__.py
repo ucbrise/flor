1c264311733d1d750bf94dace182bf11034535b0
Rolando Garcia <rogarcia@berkeley.edu>
2023-02-27 17:00:06 -0700

    bug patches

--- flor/query/__init__.py
+++ flor/query/__init__.py
@@ -90,7 +90,8 @@ def full_pivot():
         left_keys, rolling_df = pivots[0]
         for right_keys, right_df in pivots[1:]:
             rolling_df = rolling_df.merge(right_df, how="outer", on=tuple(left_keys))
-            left_keys = right_keys
+            left_keys = listright_keys
+        left_keys = list(left_keys)
         left_keys.extend([c for c in rolling_df.columns if c not in left_keys])
         return rolling_df[left_keys].sort_values(by=["tstamp", "epoch", "step"])
 
