030dc764ed12ff856aa22ec5ac0b3c684d6900b9
Rolando Garcia <rogarcia@berkeley.edu>
2023-03-23 06:19:40 -0700

    version upgrade

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with io.open("README.md", mode="r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="flordb",
-    version="2.5.14",
+    version="2.6.0",
     author="Rolando Garcia",
     author_email="rogarcia@berkeley.edu",
     description="Fast Low-Overhead Recovery",
