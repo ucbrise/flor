eb994516768e6eefba193b8b7e409e79fe05b22b
Rolando Garcia <rogarcia@berkeley.edu>
2023-03-27 01:39:06 +0000

    demo and utilities

--- flor/utils.py
+++ flor/utils.py
@@ -9,6 +9,8 @@ from .shelf import home_shelf
 
 def plot_confusion_matrix(y_true, y_pred, x_classes, y_classes):
     cf_matrix = confusion_matrix(y_true, y_pred)
+    for i in range(len(cf_matrix)):
+        cf_matrix[i][i] = 0
     df_cm = pd.DataFrame(cf_matrix, index=y_classes, columns=x_classes)
     plt.figure(figsize=(12, 7))
     sn.heatmap(df_cm, annot=False)
