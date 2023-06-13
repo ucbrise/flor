636b918c94fdab4f7725eb6427d7b80dd8a330d4
Rolando Garcia <rogarcia@berkeley.edu>
2023-06-13 15:57:38 -0500

    adding to static analysis, detect loglvl from logging stmt

--- flor/hlast/visitors.py
+++ flor/hlast/visitors.py
@@ -1,11 +1,37 @@
-from typing import Dict
+from _ast import For
+from typing import Any, Dict
 import ast
 
+def _get_log_level(i: int):
+    if i == 0:
+        return "DATA_PREP"
+    elif i == 1: 
+        return "OUTR_LOOP"
+    else:
+        return "INNR_LOOP"
 
 class LoggedExpVisitor(ast.NodeVisitor):
+    # TODO: Static analysis infers location
     def __init__(self):
         super().__init__()
         self.names: Dict[str, int] = {}
+        self.loglevels: Dict[str, str] = {}
+        
+        # LVL is DATA_PREP, OUTR_LOOP, INNR_LOOP
+        self.lvl = 0 
+
+    def visit_For(self, node: For):
+        iter_s = ast.unparse(node.iter).strip()
+        if 'Flor.loop' == iter_s[0:len('Flor.loop')] or 'MTK.loop' == iter_s[0:len('MTK.loop')]:
+            start_lvl = self.lvl
+            try:
+                self.lvl += 1
+                tree = super().visit_For(node)
+            finally:
+                self.lvl = start_lvl
+        else:
+            tree = super().visit_For(node)
+        return tree
 
     def visit_Call(self, node: ast.Call):
         pred = (
@@ -17,7 +43,9 @@ class LoggedExpVisitor(ast.NodeVisitor):
         if not pred:
             return self.generic_visit(node)
         if len(node.args) == 2 and isinstance(node.args[0], ast.Constant):
-            self.names[str(node.args[0].value)] = node.lineno
+            n = str(node.args[0].value)
+            self.names[n] = node.lineno
+            self.loglevels[n] = _get_log_level(self.lvl)
         else:
             raise IndexError("FLOR: Did you give flor.log a key? It takes 2 args.")
 
