421f2270fcf66d691d4d0696200b35554be9b82f
Rolando Garcia <rogarcia@berkeley.edu>
2023-08-08 11:41:26 -0500

    saving api changes

--- flor/api.py
+++ flor/api.py
@@ -67,8 +67,9 @@ def checkpointing(*args):
 
 
 def layer(name: str, iterator: Iterable[T]) -> Iterator[T]:
+    pos = len(layers)
     layers[name] = 0
-    for each in tqdm(iterator):
+    for each in tqdm(iterator, position=pos, leave=(True if pos == 0 else False)):
         layers[name] += 1
         yield each
 
