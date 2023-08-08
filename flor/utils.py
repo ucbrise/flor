ea5c80701da8a0e205427877de1b11e61ecb6496
Rolando Garcia <rogarcia@berkeley.edu>
2023-08-07 21:00:45 -0500

    saving work

--- flor/utils.py
+++ flor/utils.py
@@ -6,3 +6,23 @@ def is_jsonable(x):
             return True
         except (TypeError, OverflowError):
             return False
+        
+def duck_cast(v, default):
+    if isinstance(default, bool):
+        return bool(v)
+    if isinstance(default, int):
+        return int(v)
+    elif isinstance(default, float):
+        return float(v)
+    elif isinstance(default, str):
+        return str(v) if v else ""
+    elif isinstance(default, list):
+        return list(v) if v else []
+    elif isinstance(default, tuple):
+        return tuple(v) if v else tuple([])
+    elif isinstance(default, bytes):
+        return bytes(v)
+    elif isinstance(default, bytearray):
+        return bytearray(v)
+    else:
+        raise TypeError(f"Unsupported type: {type(default)}")
