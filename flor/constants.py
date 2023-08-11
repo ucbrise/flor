f1e19a90da7c0847d9d662c567ef08972876fa2d
Rolando Garcia <rogarcia@berkeley.edu>
2023-08-11 10:20:11 -0500

    cleanup

--- flor/constants.py
+++ flor/constants.py
@@ -15,4 +15,12 @@ HOMEDIR = os.path.join(os.path.expanduser("~"), ".flor")
 SHADOW_BRANCH_PREFIX = "flor."
 SCRIPTNAME = os.path.basename(sys.argv[0])
 
-__all__ = ["SHADOW_BRANCH_PREFIX", "STEM", "TIMESTAMP", "CURRDIR", "PROJID", "HOMEDIR", "SCRIPTNAME"]
+__all__ = [
+    "SHADOW_BRANCH_PREFIX",
+    "STEM",
+    "TIMESTAMP",
+    "CURRDIR",
+    "PROJID",
+    "HOMEDIR",
+    "SCRIPTNAME",
+]
