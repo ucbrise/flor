7bf5215326a6db944764d2c37228171da547e1ae
Rolando Garcia <rogarcia@berkeley.edu>
2023-08-14 14:23:45 -0500

    saving work

--- flor/obj_store.py
+++ flor/obj_store.py
@@ -14,7 +14,7 @@ def serialize_torch(layers, name, obj):
     import torch.optim
 
     if isinstance(obj, torch.nn.Module) or isinstance(obj, torch.optim.Optimizer):
-        path = SHELF / utils.to_filename(layers, name, ".pth")
+        path = get_shelf() / utils.to_filename(layers, name, ".pth")
         torch.save(obj.state_dict(), path)
         return path.name
     else:
@@ -25,7 +25,7 @@ def serialize_numpy(layers, name, obj):
     import numpy as np
 
     if isinstance(obj, np.ndarray):
-        path = SHELF / utils.to_filename(layers, name, ".npy")
+        path = get_shelf() / utils.to_filename(layers, name, ".npy")
         np.save(path, obj)
         return path.name
     else:
@@ -44,7 +44,7 @@ def serialize_scikit(layers, name, obj):
         sklearn.base.TransformerMixin,
     )
     if isinstance(obj, sklearn_base_classes) or hasattr(obj, "fit"):
-        path = SHELF / utils.to_filename(layers, name, ".pkl")
+        path = get_shelf() / utils.to_filename(layers, name, ".pkl")
         with open(path, "wb") as f:
             pickle.dump(obj, f)
         return path.name
@@ -56,7 +56,7 @@ def serialize_pandas(layers, name, obj):
     import pandas as pd
 
     if isinstance(obj, pd.DataFrame):
-        path = SHELF / utils.to_filename(layers, name, ".parquet")
+        path = get_shelf() / utils.to_filename(layers, name, ".parquet")
         obj.to_parquet(path)
         return path.name
     else:
@@ -64,11 +64,6 @@ def serialize_pandas(layers, name, obj):
 
 
 def serialize(layers, name, obj):
-    global SHELF
-    OBJSTORE = Path(HOMEDIR) / "obj_store"
-    SHELF = OBJSTORE / PROJID / TIMESTAMP
-    os.makedirs(SHELF, exist_ok=True)
-
     try:
         return serialize_torch(layers, name, obj)
     except:
@@ -89,31 +84,43 @@ def serialize(layers, name, obj):
     except:
         pass
 
-    with open((path := SHELF / utils.to_filename(layers, name, ".pkl")), "wb") as f:
+    with open(
+        (path := get_shelf() / utils.to_filename(layers, name, ".pkl")), "wb"
+    ) as f:
         cloudpickle.dump(obj, f)
     return path.name
 
 
 def deserialize(layers, name, obj):
-    global SHELF
-    OBJSTORE = Path(HOMEDIR) / "obj_store"
-    SHELF = OBJSTORE / PROJID / cli.flags.hyperparameters["TSTAMP"]
-    if (path := SHELF / utils.to_filename(layers, name, ".pth")).exists():
+    if (path := get_shelf() / utils.to_filename(layers, name, ".pth")).exists():
         import torch
 
         obj.load_state_dict(torch.load(path))
-    elif (path := SHELF / utils.to_filename(layers, name, ".npy")).exists():
+    elif (path := get_shelf() / utils.to_filename(layers, name, ".npy")).exists():
         import numpy
 
         obj[:] = numpy.load(path)
-    elif (path := SHELF / utils.to_filename(layers, name, ".parquet")).exists():
+    elif (path := get_shelf() / utils.to_filename(layers, name, ".parquet")).exists():
         import pandas as pd
 
         obj.iloc[:, :] = pd.read_parquet(path)
-    elif (path := SHELF / utils.to_filename(layers, name, ".pkl")).exists():
+    elif (path := get_shelf() / utils.to_filename(layers, name, ".pkl")).exists():
         with open(path, "rb") as f:
             loaded_obj = cloudpickle.load(f)
         obj.clear()
         obj.update(loaded_obj)
     else:
         raise
+
+
+def get_shelf():
+    if not cli.in_replay_mode():
+        tstamp = TIMESTAMP
+    else:
+        assert cli.flags.old_tstamp is not None
+        tstamp = cli.flags.old_tstamp
+
+    OBJSTORE = Path(HOMEDIR) / "obj_store"
+    SHELF = OBJSTORE / PROJID / tstamp
+    os.makedirs(SHELF, exist_ok=True)
+    return SHELF
