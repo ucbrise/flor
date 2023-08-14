a4ec6ca0660da0ca83e4d55a3851ed66d81aad7d
Rolando Garcia <rogarcia@berkeley.edu>
2023-08-14 11:40:02 -0500

    saving work

--- flor/cli.py
+++ flor/cli.py
@@ -46,7 +46,8 @@ def parse_args():
 
     args = parser.parse_args()
 
-    flags.queryparameters = args.replay_flor
+    for d in args.replay_flor:
+        flags.queryparameters.update(d)
 
     # Process the key-value pair arguments
     if args.kwargs is not None:
