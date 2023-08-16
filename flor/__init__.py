fad96cb012c5adb692f59709202f1f49bbc2cfa2
Rolando Garcia <rogarcia@berkeley.edu>
2023-08-16 12:32:25 -0500

    moving flor shadow branch initialization to deferred_init

--- flor/__init__.py
+++ flor/__init__.py
@@ -4,8 +4,3 @@ from . import cli
 from . import versions
 
 cli.parse_args()
-
-if cli.flags.args is not None:
-    if cli.flags.args.flor_command is None:
-        if versions.current_branch() is not None:
-            versions.to_shadow()
