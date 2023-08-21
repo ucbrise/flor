2f9470b3ef5a560d52c509d6e421643246379169
Rolando Garcia <rogarcia@berkeley.edu>
2023-08-21 15:56:27 -0500

    cleanup

--- flor/hlast/visitors.py
+++ flor/hlast/visitors.py
@@ -1,36 +1,10 @@
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 import ast
 
 class LoggedExpVisitor(ast.NodeVisitor):
     def __init__(self):
         super().__init__()
-        self.names: Dict[str, int] = {}
-        self.loglevels: Dict[str, int] = {}
-        
-        self.lvl = 0 
-
-    def get_loglevel(self, filtered_keys):
-        i = max({k:self.loglevels[k] for k in (filtered_keys if filtered_keys is not None else self.loglevels)}.values())
-        if i == 0:
-            return "DATA_PREP"
-        elif i == 1:
-            return "OUTR_LOOP"
-        elif i >= 2:
-            return "INNR_LOOP"
-        raise
-
-    def visit_For(self, node: ast.For):
-        iter_s = ast.unparse(node.iter).strip()
-        if 'Flor.loop' == iter_s[0:len('Flor.loop')] or 'MTK.loop' == iter_s[0:len('MTK.loop')]:
-            start_lvl = self.lvl
-            try:
-                self.lvl += 1
-                tree = self.generic_visit(node)
-            finally:
-                self.lvl = start_lvl
-        else:
-            tree = self.generic_visit(node)
-        return tree
+        self.name: Optional[str] = None
 
     def visit_Call(self, node: ast.Call):
         pred = (
@@ -42,9 +16,7 @@ class LoggedExpVisitor(ast.NodeVisitor):
         if not pred:
             return self.generic_visit(node)
         if len(node.args) == 2 and isinstance(node.args[0], ast.Constant):
-            n = str(node.args[0].value)
-            self.names[n] = node.lineno
-            self.loglevels[n] = self.lvl
+            self.name = str(node.args[0].value)
         else:
             raise IndexError("FLOR: Did you give flor.log a key? It takes 2 args.")
 
@@ -82,9 +54,9 @@ class NoGradVisitor(ast.NodeVisitor):
                 self.feeding = feeding  # type: ignore
 
 class NoGradTransformer(ast.NodeTransformer):
-    def __init__(self, their_tree) -> None:
+    def __init__(self, old_tree) -> None:
         super().__init__()
-        self.their_tree = their_tree
+        self.their_tree = old_tree
 
     def visit_With(self, node: ast.With):
         if [True for each in node.items if "torch.no_grad" in ast.unparse(each)]:
