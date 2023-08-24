9137e72b1c9203c2fd35f8de4bfb8ba0c7c5afe2
Rolando Garcia <rogarcia@berkeley.edu>
2023-08-24 10:52:50 -0500

    .

--- flor/repl.py
+++ flor/repl.py
@@ -31,6 +31,8 @@ def replay(apply_vars: List[str], where_clause: Optional[str]):
     print("where_clause", where_clause)
 
 
+__all__ = ["pivot", "query", "replay"]
+
 # def apply(names: List[str], dst: str, stash=None):
 #     """
 #     Caller checks out a previous version
