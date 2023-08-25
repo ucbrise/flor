1b00ff26610219b1faf782cb5f9de26ee6cc0495
Rolando Garcia <rogarcia@berkeley.edu>
2023-08-25 13:52:10 -0500

    saving work

--- flor/orm.py
+++ flor/orm.py
@@ -24,36 +24,37 @@ class Log:
     value: Any
     type: Val_Type
 
-
-entries: Dict[str, int] = {}
-logs: List[Log] = []
-loops: Dict[Tuple[int, str, int, int], int] = {}
-
-
-def parse_log(stem, obj, loop):
-    log_record = Log(
-        stem["PROJID"],
-        stem["TSTAMP"],
-        stem["FILENAME"],
-        loop,
-        obj["value_name"],
-        obj["value"],
-        Val_Type.AUTO if "::" in obj["value_name"] else Val_Type.RAW,
-    )
-    logs.append(log_record)
-    return log_record
+class ORM:
+    def __init__(self) -> None:
+        self.entries: Dict[str, int] = {}
+        self.logs: List[Log] = []
+        self.loops: Dict[Tuple[int, str, int, int], int] = {}
 
 
-def parse_loop(obj):
-    loop = None
-    for k, v in entries.items():
-        if k in obj:
-            loop = Loop(loop, k, v, obj[k])
-    return loop
+    def parse_log(self, stem, obj, loop):
+        log_record = Log(
+            stem["PROJID"],
+            stem["TSTAMP"],
+            stem["FILENAME"],
+            loop,
+            obj["value_name"],
+            obj["value"],
+            Val_Type.AUTO if "::" in obj["value_name"] else Val_Type.RAW,
+        )
+        self.logs.append(log_record)
+        return log_record
 
 
-def parse_entries(obj):
-    if obj["value_name"].startswith("enter::"):
-        entries[obj["value_name"].split("::")[1]] = (
-            entries.get(obj["value_name"].split("::")[1], 0) + 1
+    def parse_loop(self, obj):
+        loop = None
+        for k, v in self.entries.items():
+            if k in obj:
+                loop = Loop(loop, k, v, obj[k])
+        return loop
+
+
+    def parse_entries(self, obj):
+        if obj["value_name"].startswith("enter::"):
+            self.entries[obj["value_name"].split("::")[1]] = (
+                self.entries.get(obj["value_name"].split("::")[1], 0) + 1
         )
