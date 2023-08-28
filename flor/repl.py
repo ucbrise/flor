632bbd7929184e7b6e80d08a726082d91a9d837a
Rolando Garcia <rogarcia@berkeley.edu>
2023-08-28 09:52:47 -0500

    saving work

--- flor/repl.py
+++ flor/repl.py
@@ -144,6 +144,7 @@ def get_schedule(apply_vars, where_clause):
             common_columns = set(df.columns) & set(ext_df.columns)
             df = pd.merge(df, ext_df, on=list(common_columns), how="outer")
         schedule = df.query(where_clause)
+        schedule = schedule.copy()
         for var in [v for v in apply_vars if v not in schedule.columns]:
             schedule.loc[:,var] = np.nan
         return schedule
