0791b090c4c8f76a84a44592dc92e831c3b7f787
Rolando Garcia <rogarcia@berkeley.edu>
2023-11-28 12:14:09 -0600

    creating new directory

--- flor/constants.py
+++ flor/constants.py
@@ -5,6 +5,7 @@ import sys
 CURRDIR = os.getcwd()
 PROJID = os.path.basename(CURRDIR)
 HOMEDIR = os.path.join(os.path.expanduser("~"), ".flor")
+os.makedirs(HOMEDIR, exist_ok=True)
 SHADOW_BRANCH_PREFIX = "flor."
 SCRIPTNAME = os.path.basename(sys.argv[0])
 TIMESTAMP = datetime.now().isoformat(timespec="seconds")
