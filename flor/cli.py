fd8923dd01c2ec9752a373086376e259e393f3a4
Rolando Garcia <rogarcia@berkeley.edu>
2024-01-11 13:52:28 -0600

    Add dataframe functionality to flor CLI

--- flor/cli.py
+++ flor/cli.py
@@ -5,6 +5,7 @@ from typing import Any, Dict, Optional, Tuple
 
 from dataclasses import dataclass
 from .versions import current_branch, to_shadow
+from .repl import dataframe
 import sys
 
 
@@ -111,6 +112,10 @@ def parse_args():
 
     if flags.args is not None and flags.args.flor_command == "dataframe":
         flags.columns = flags.args.columns
+        if flags.columns is None:
+            print(dataframe())
+        else:
+            print(dataframe(*flags.columns))
 
     return flags
 
