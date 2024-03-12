e6677202f62c239c93395217a06e7923b0ad0c4b
Rolando Garcia <rogarcia@berkeley.edu>
2024-03-12 10:36:54 -0500

    Add import statement and update file path for JSON output

--- flor/orm.py
+++ flor/orm.py
@@ -1,7 +1,10 @@
 from dataclasses import dataclass, asdict
 from typing import Any, List, Optional
 import json
+import os
 import random
+
+from . import versions
 
 
 def generate_64bit_id() -> int:
@@ -30,5 +33,5 @@ class Log:
 
 def to_json(output_buffer: List[Log]):
     buffer = [asdict(o) for o in output_buffer]
-    with open(".flor.json", "w") as f:
+    with open(os.path.join(versions.get_repo_dir(), ".flor.json"), "w") as f:
         json.dump(buffer, f, indent=2)
